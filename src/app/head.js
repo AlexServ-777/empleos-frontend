32a847ef5f079f5f20cc06d139077afbf9e4b024
alexServ <alexdaniel2003.yt@gmail.com>
2025-06-17 18:18:53 -0400

    google ads

--- src/app/head.js
+++ src/app/head.js
@@ -1,6 +1,7 @@
 export default function Head(){
     return(
         <>
+            <meta name="google-adsense-account" content="ca-pub-7383764596596086"></meta>
             <link rel="icon" href="/favicon.ico"/>
         </>
     )
